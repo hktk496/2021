38ada9921a9c9557598a8f95d3bf11ac741b6b64
hktk496 <1546521798@qq.com>
2021-01-19 21:34:39 +0800

    提交代码

---
+++ tset_1_13/tset_1_13/tset.c
@@ -0,0 +1,35 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	printf("ÇëÊäÈëÄãµÄÖÇÉÌ:\n");
+//	scanf("%d", &a);
+//	if (a >= 140)
+//	{
+//		printf("Genius");
+//	};
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b = 0;
+//	scanf("%d%d", &a,&b);
+//	int c = a / b;
+//	int d = a%b;
+//	printf("%d", c);
+//	printf("%d", d);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10, b = 5;
+//	if (a >= b)
+//		printf("%d", a);
+//	else
+//		printf("%d", b);
+//
+//	return 0;
+//}
