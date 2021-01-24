148c6e0315465200c8402ef7f780bbc34800b4f5
hktk496 <1546521798@qq.com>
2021-01-24 22:26:52 +0800

    提交

---
+++ test_1_24/test_1_24/test.c
@@ -0,0 +1,45 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	char arr[40][40] = {'*'};
+//	//for (int x = 0; x < 20; x++)
+//	//{
+//	//	for (int y = 0; y < 20; y++)
+//	//	{
+//	//		if ((((x ^ 2) + (y ^ 2) - 1) ^ 3) == (((x ^ 2) * (y ^ 3))))
+//	//			arr[x][y] = '*';
+//	//	}
+//	//}
+//	for (int x = 0; x < 40; x++)
+//	{
+//		for (int y = 0; y < 40; y++)
+//		{
+//			if ((x*x + y*y - 1)*(x*x + y*y - 1)*(x*x + y*y - 1) == (x*x*y*y*y))
+//				continue;
+//			arr[x][y] =  '*' ;
+//			printf("%c ", arr[x][y]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//#include <stdio.h>  
+//int main()
+//{
+//	for (float y = 1.5f; y > -1.5f; y -= 0.1f)
+//	{
+//		for (float x = -1.5f; x < 1.5f; x += 0.05f)
+//		{
+//			float a = x * x + y * y - 1;
+//			putchar(a * a * a - x * x * y * y * y <= 0.0f ? '*' : ' ');
+//		}
+//		putchar('\n');
+//	}
+//}
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
